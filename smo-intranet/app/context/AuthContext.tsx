7128463f04ab3d0622a2a0fa9471f65534e003a0
Vicente <vicente.orlandini@gmail.com>
2026-06-19 00:32:02 -0400

    Implementaciones de Auth
    se implementan funciones para el registro de autoria en login

---
+++ smo-intranet/app/context/AuthContext.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import {useContext, useState, createContext} from 'react'
+
+const AuthContext = createContext(null)
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+
+// user es para el usuario que se loguea y setUser es para verificar si esta en el local storage.
+const [user, setUser] = useState(() =>
+  JSON.parse(localStorage.getItem('user') || 'null')
+)
+
+const login = (email: string, password: string) => { // verifica email y pass como strings y si coinciden con lo establecido accede
+    if (email === 'Vicente@test.com' && password === '123456') {
+      const userData = { email }
+      localStorage.setItem('user', JSON.stringify(userData))
+      setUser(userData)
+      return true
+    }
+    return false
+  }
+
+const logout = () => { // Elimina al user del localstorage y lo cambia a un null
+    localStorage.removeItem('user')
+    setUser(null)
+    }
+
+return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+    {children}
+    </AuthContext.Provider>
+)
+}
